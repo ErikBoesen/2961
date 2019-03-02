7509c986054364b05a753c26e837e9b38fe60e37
ErikBoesen <me@erikboesen.com>
2019-03-02 14:34:38 -0500

    Add camera

---
+++ robot/camera/camera.py
@@ -0,0 +1,23 @@
+from cscore import CameraServer, UsbCamera
+from networktables import NetworkTables
+
+
+def main():
+    cs = CameraServer.getInstance()
+    cs.enableLogging()
+    usb0 = cs.startAutomaticCapture(dev=0)
+    cs.waitForever()
+
+
+if __name__ == "__main__":
+
+    # To see messages from networktables, you must setup logging
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    # You should uncomment these to connect to the RoboRIO
+    # import networktables
+    # networktables.initialize(server='10.xx.xx.2')
+
+    main()
