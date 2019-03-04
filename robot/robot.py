5a500bc68c566aabd90d572d02b9c4c76f173ea3
ErikBoesen <me@erikboesen.com>
2019-03-03 21:30:18 -0500

    Add comments in createObjects

--- robot/robot.py
+++ robot/robot.py
@@ -13,24 +13,42 @@ class Robot(magicbot.MagicRobot):
     grab = False
     request_grab = False
     def createObjects(self):
-        # For teleop auto
+        # For teleop autonomous, we have to do this so that the autonomous can call teleopPeriodic.
         self.robot = self
 
+        # Create an object for the Xbox controller. You can now use self.controller elsewhere to get
+        # data from the controller like buttons, axes, etc. just as you would imagine.
         self.controller = wpilib.XboxController(0)
+
+        # Create objects for each Talon motor controller. You must pass in the number of the
+        # PWM port being used so that the code knows which Talon you want.
         self.lf_motor = wpilib.Talon(1)
         self.lr_motor = wpilib.Talon(0)
         self.rf_motor = wpilib.Talon(3)
         self.rr_motor = wpilib.Talon(2)
 
+        # Create a drivetrain object. What this line is essentially doing is making a grouping of
+        # all the motors on the left, one for all the motors on the right, and then passing both
+        # of them in to wpilib.drive.DifferentialDrive, which handles all the calculations for driving
+        # using tank drive so you don't have to do tons of unnecessary math yourself.
         self.train = wpilib.drive.DifferentialDrive(wpilib.SpeedControllerGroup(self.lf_motor, self.lr_motor),
                                                     wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor))
 
+        # Create object for the solenoid you're using to extend out the hatch grabber, and another
+        # for the solenoid you're using to actually grab the hatch.
         self.extend_solenoid = wpilib.DoubleSolenoid(0, 1)
         self.grab_solenoid = wpilib.DoubleSolenoid(2, 3)
 
+        # Create two objects for the buttons you're using to control extending and grabbing.
+        # You can technically use functions like self.controller.getAButton() to check whether
+        # a certain button is pressed, but I prefer this method because then you can name your
+        # buttons and it is a little bit clearer what button is what.
         self.button_hatch = JoystickButton(self.controller, 5)  # left bumper button
         self.button_grab = JoystickButton(self.controller, 1)  # A
+        # If you need to check what button is what on your controller, reference this page in the RobotPy
+        # documentation: https://robotpy.readthedocs.io/projects/wpilib/en/latest/wpilib/XboxController.html
 
+        # Start sending your camera feed to the driverstation
         wpilib.CameraServer.launch('camera/camera.py:main')
         wpilib.LiveWindow.disableAllTelemetry()
 
